0f145727dfcada1e661fb31acf8e4b7654851522
APTEMmm <artemqrtz@gmail.com>
2022-09-17 23:33:42 +0300

    Updated `#suitable_clothing` tests

--- spec/lib/things_collection_spec.rb
+++ spec/lib/things_collection_spec.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require './lib/things_collection'
+require './lib/thing'
 require 'rspec'
 
 RSpec.describe ThingsCollection do
-  let(:things_collection) { described_class.new([]) }
+  let(:thing1) { Thing.new('Шапка-ушанка', 'Головной убор', '(-20, -5)') }
+  let(:thing2) { Thing.new('Шлепанцы', 'Обувь', '(+20, +40)') }
+  let(:things_collection) { described_class.new([thing1, thing2]) }
 
   describe '.from_dir' do
     it 'should return ThingCollection instance' do
@@ -13,8 +16,11 @@ RSpec.describe ThingsCollection do
   end
 
   describe '#suitable_clothing' do
-    it 'should return array' do
-      expect(things_collection.suitable_clothing(10)).to be_a Array
+    let(:thing_for_plus20) { ['Головной убор: Нечего порекомендовать', 'Обувь: Шлепанцы (+20, +40)'] }
+    let(:thing_for_minus10) { ['Головной убор: Шапка-ушанка (-20, -5)', 'Обувь: Нечего порекомендовать'] }
+    it 'should return ' do
+      expect(things_collection.suitable_clothing(20)).to eq thing_for_plus20
+      expect(things_collection.suitable_clothing(-10)).to eq thing_for_minus10
     end
   end
 end
